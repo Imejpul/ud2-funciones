3649e0765427832a7074e44b98e1ca2fa677b878
Unknown <ivanmejiapulido@gmail.com>
2017-11-20 20:21:22 +0100

    Ejercicio 9 (recursivo resuelto)

--- 09_fibonacci/fibonacci/src/com/imejpul/Main.java
+++ 09_fibonacci/fibonacci/src/com/imejpul/Main.java
@@ -1,31 +1,44 @@
 package com.imejpul;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static int fibonacci_iterativo(int n){
+    public static int fibonacci_iterativo(int n) {
         return 0;
     }
 
-    public static int fibonacci_recursivo(int n){
+    public static int fibonacci_recursivo(int n) {
 
-        if (n > 2){
+        if (n > 2) {
             return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2);
 
-        }else {
+        } else {
 
             return n;
         }
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-       // System.out.println(fibonacci_iterativo(7));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 15; i++) {
-            System.out.print(fibonacci_recursivo(i)+" ");
+        // System.out.println(fibonacci_iterativo(7));
 
+        //-----------------------------------MODO RECURSIVO----------------------------------------
+
+        System.out.print("Introduce el número de termino de la sucesión de fibonacci a calcular: ");
+        int termino = Integer.parseInt(br.readLine());
+        int fibTermino = 0;
+
+        for (int i = 0; i < termino; i++) {
+            fibTermino = fibonacci_recursivo(i);
         }
+
+        System.out.println(fibTermino);
 
     }
 }
